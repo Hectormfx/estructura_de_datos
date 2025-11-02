e5970d7ebdb3effa1f1f4c463bb421345b71c5b9
Hectormfx <hectormfx10@gmail.com>
2025-11-02 13:35:19 -0700

    Modificaciones de practica
    Se modifico lo siguiente:
    1. Migramos a vector int
    2. Creamos el vector con 200 valores
    3. Aseguramos que los valroes sean aleatorios, unicos y en el rango pedido

--- codigo/Practica3.cpp
+++ codigo/Practica3.cpp
@@ -1,17 +1,22 @@
 #include <iostream>   // Biblioteca estándar de C++ que permite usar funciones de entrada y salida como cout y cin
 #include <iomanip>    // Biblioteca que permite manipular el formato de salida (por ejemplo setw para alinear texto)
+#include <vector>     //Biblioteca para trabar con vectores en lugar del array
+#include <random>     // Biblioteca para generar numeros aleatorias
+#include <algorithm>  //Biblioteca para usar find por ejemmplo
 using namespace std;  // Permite usar cout, cin y endl sin escribir el prefijo std:: antes de cada uno
 
 
 // Función para mostrar el contenido del array
 
-void mostrarArray(int arr[], int tam) {   // Función que recibe un arreglo y su tamaño como parámetros
-    cout << "CONTENIDO DEL ARRAY:\n";     // Imprime un encabezado para identificar el arreglo
+//CAMBIO A LA FUNCIÓN mostrarArray: pasamos el vector por referencia constante  (no se copia y no lo modificamos), tambien ya no usamos tam, porque ahora usamos arr.size() esto nos da el tamaño de array
+
+void mostrarArray(const vector<int>& arr) {   // Función que recibe un arreglo y su tamaño como parámetros
+    cout << "CONTENIDO DEL ARRAY (VECTOR):\n";     // Imprime un encabezado para identificar el arreglo
     cout << "\n";                         // Imprime una línea vacía para separar visualmente
     
-    for (int i = 0; i < tam; i++) {       // Ciclo for para recorrer todos los elementos del arreglo
+    for (int i = 0; i < arr.size(); i++) {       // Ciclo for para recorrer todos los elementos del arreglo
         cout << setw(3) << arr[i];        // Muestra cada elemento con un ancho fijo de 3 caracteres (setw viene de <iomanip>)
-        if (i < tam - 1) cout << ", ";    // Si no es el último elemento, imprime una coma como separador
+        if (i < arr.size() - 1) cout << ", ";    // Si no es el último elemento, imprime una coma como separador
     }
     cout << "\n\n";                       // Imprime dos saltos de línea para dejar espacio después del arreglo
 }
@@ -19,18 +24,20 @@ void mostrarArray(int arr[], int tam) {   // Función que recibe un arreglo y su
 
 // Función que realiza la búsqueda secuencial
 
-void busquedaSecuencial(int arr[], int tam, int clave) {  // Función que busca un valor dentro del arreglo
-    bool encontrado = false;   // Variable booleana para indicar si la clave se encontró
+//CAMBIO A LA FUNCIÓN busquedaSecuencial
+
+bool busquedaSecuencial(const vector<int>& arr, int clave, int &posicion) {  // Función que busca un valor dentro del arreglo
     int posicion = -1;         // Variable para guardar la posición donde se encuentra la clave
     
     // Recorre cada elemento del arreglo
-    for (int i = 0; i < tam; i++) {     
+    for (int i = 0; i < arr.size(); i++) {     
         if (arr[i] == clave) {      // Compara el valor actual con la clave buscada
-            encontrado = true;      // Si se encuentra, cambia la bandera a verdadero
             posicion = i;           // Guarda la posición donde se encontró el valor
-            break;                  // Se detiene el ciclo (optimiza el tiempo de ejecución)
+            return true;                  // Se encontro
         }
     }
+
+    return false; // No se encontro
     
     // Muestra los resultados de la búsqueda
     cout << "\n--- RESULTADO BUSQUEDA SECUENCIAL ---\n";   // Imprime un encabezado
@@ -45,48 +52,63 @@ void busquedaSecuencial(int arr[], int tam, int clave) {  // Función que busca 
 
 // Función que realiza la búsqueda binaria
 
-void busquedaBinaria(int arr[], int tam, int clave) {   // Función que implementa el algoritmo de búsqueda binaria
+//MODIFICACIONES A LA BUSQUEDA BINARIA
+
+bool busquedaBinaria(const vector<int>& arr, int clave, int &posicion) {   // Función que implementa el algoritmo de búsqueda binaria
     int limiteInferior = 0;        // Primer índice del arreglo (inicio)
-    int limiteSuperior = tam - 1;  // Último índice del arreglo (fin)
-    int mitad;                     // Variable para calcular la posición media
-    bool encontrado = false;       // Bandera para saber si se halló la clave
+    int limiteSuperior = arr.size() - 1;  // Último índice del arreglo (fin)
     int posicion = -1;             // Variable para guardar el índice donde se encontró
     
-    cout << "\n--- PROCESO DE BUSQUEDA BINARIA ---\n";  // Imprime encabezado explicativo
+    //cout << "\n--- PROCESO DE BUSQUEDA BINARIA ---\n";  // Imprime encabezado explicativo
     
     // Mientras los límites sean válidos y no se haya encontrado el elemento
-    while (limiteInferior <= limiteSuperior && !encontrado) {
-        mitad = (limiteInferior + limiteSuperior) / 2;   // Calcula la posición media (mitad del rango actual)
+    while (limiteInferior <= limiteSuperior) {
+        int mitad = (limiteInferior + limiteSuperior) / 2;   // Calcula la posición media (mitad del rango actual)
         
+        if (arr[mitad] == clave) {
+            posicion = mitad;
+            return true; //encontrado
+        }
+        else if (clave < arr[mitad]) {
+            limiteSuperior = mitad - 1;
+
+        }
+        else {
+            limiteInferior = mitad + 1;
+        }
+        
+       //LA VERSIÓN CON MENSAJES DE SALIDA LA VAMOS A OMITIR POR LA CUESTION DE VELOCIDAD, PERO LA DEJAREMOS GUARDADA AQUI EN FORMA DE COMENTARIO
+       
         // Muestra los valores de los límites y el elemento central
-        cout << "Limite Inferior: " << limiteInferior 
-             << " | Limite Superior: " << limiteSuperior 
-             << " | Mitad: " << mitad 
-             << " | Valor en mitad: " << arr[mitad] << endl;
+       // cout << "Limite Inferior: " << limiteInferior 
+       //      << " | Limite Superior: " << limiteSuperior 
+       //      << " | Mitad: " << mitad 
+       //      << " | Valor en mitad: " << arr[mitad] << endl;
         
         // Compara la clave buscada con el elemento central
-        if (arr[mitad] == clave) {             // Si el elemento central es igual a la clave
-            encontrado = true;                 // Marca que se encontró
-            posicion = mitad;                  // Guarda la posición
-        } else if (clave < arr[mitad]) {       // Si la clave es menor que el valor central
-            limiteSuperior = mitad - 1;        // Ajusta el límite superior (busca en la mitad izquierda)
-        } else {                               // Si la clave es mayor que el valor central
-            limiteInferior = mitad + 1;        // Ajusta el límite inferior (busca en la mitad derecha)
-        }
+       // if (arr[mitad] == clave) {             // Si el elemento central es igual a la clave
+       //     encontrado = true;                 // Marca que se encontró
+       //     posicion = mitad;                  // Guarda la posición
+       //} else if (clave < arr[mitad]) {       // Si la clave es menor que el valor central
+       //     limiteSuperior = mitad - 1;        // Ajusta el límite superior (busca en la mitad izquierda)
+       // } else {                               // Si la clave es mayor que el valor central
+       //     limiteInferior = mitad + 1;        // Ajusta el límite inferior (busca en la mitad derecha)
+       // }
     }
     
+    return false; //no encontrado
     // Muestra los resultados finales de la búsqueda
-    cout << "\n--- RESULTADO BUSQUEDA BINARIA ---\n";  // Encabezado de resultado
-    if (encontrado) {                                  // Si se encontró el valor
-        cout << "Se encontro el valor buscado en la posicion: " << posicion << endl;
-        cout << "El limite inferior es: " << limiteInferior << endl;
-        cout << "El limite superior es: " << limiteSuperior << endl;
-    } else {                                           // Si no se encontró
-        cout << "No se encontro el valor buscado\n";
-        cout << "Ultimo limite inferior: " << limiteInferior << endl;
-        cout << "Ultimo limite superior: " << limiteSuperior << endl;
-    }
-    cout << "-----------------------------------\n\n"; // Línea separadora
+   // cout << "\n--- RESULTADO BUSQUEDA BINARIA ---\n";  // Encabezado de resultado
+    //if (encontrado) {                                  // Si se encontró el valor
+   //     cout << "Se encontro el valor buscado en la posicion: " << posicion << endl;
+   //     cout << "El limite inferior es: " << limiteInferior << endl;
+   //     cout << "El limite superior es: " << limiteSuperior << endl;
+   // } else {                                           // Si no se encontró
+   //     cout << "No se encontro el valor buscado\n";
+   //     cout << "Ultimo limite inferior: " << limiteInferior << endl;
+   //     cout << "Ultimo limite superior: " << limiteSuperior << endl;
+   // }
+   //cout << "-----------------------------------\n\n"; // Línea separadora
 }
 
 
@@ -105,8 +127,29 @@ void mostrarMenu() {
 // Función principal del programa (punto de inicio)
 
 int main() {
-    // Se crea el arreglo con los valores dados en el problema
-    int arr[] = {5, 8, 10, 13, 15, 18, 20, 23, 25, 29, 32, 36, 40, 42, 47, 51, 55, 63, 66};
+    // Crear todos los posibles valores de 0 a 600
+    vector<int> universo;
+    for (int x = 0; x <= 600; x++) {
+        universo.push_back(x);
+    }
+
+    //resolver el vector universo con suffle
+    random_device rd; //semilla del hardware
+    mt19937 gen(rd()); //motor aleatorio mersenne twister
+    shuffle(universo.begin(), universo.end(), gen);
+
+    //tomar los primeros 200 datos ya mezclados
+    vector<int> datos(200);
+    for (int i = 0; i < 200; i++) {
+        datos[i] = universo[i];
+    }
+
+    //MOSTRAR EL VECTOR GENERADO ALEATOREAMENTE (200 valores unios entre 0 y 600)
+
+    cout << "VECTOR GENERADO ALEATORIAMENTE (200 valores unicos entre 0 y 600): \n";
+    mostrarArray(datos);
+    
+    
     
     // Se calcula el tamaño del arreglo usando sizeof:
     // sizeof(arr) devuelve el tamaño total del arreglo en bytes
